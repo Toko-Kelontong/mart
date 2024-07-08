e8e417dcd6946bab3ebfbfe393e272cbde62d345
Juniartha <113610040+Juniartha@users.noreply.github.com>
2024-07-08 22:01:18 +0800

    add table product, order, catrgory

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,5 @@
+
+import { float} from "drizzle-orm/mysql-core";
 import {
     pgTable,
     varchar,
@@ -5,7 +7,10 @@ import {
     text,
     timestamp,
     uuid,
+    integer,
+    real,
 } from "drizzle-orm/pg-core";
+
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey(),
@@ -26,8 +31,43 @@ export const roles = pgTable("roles", {
     updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
+export const product = pgTable("product", {
+    id: serial("id").primaryKey().notNull(),
+    name: varchar("name", { length: 255 }).notNull(),
+    price: integer("price").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(),
+    image: text("image").notNull(),
+    descriptions: text("descriptions"),
+    category_id: uuid("category_id").references(() => category.id),
+});
+
+export const category = pgTable("category", {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    descriptions: text("descriptions"),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const order = pgTable("order", {
+    id: uuid("id").primaryKey(),
+    order_date: timestamp("order_at").notNull().defaultNow(),
+    total_price: real("total_price").notNull(),
+    user_id: uuid("user_id").references(() => users.id)
+});
+
 export type InsertUser = typeof users.$inferInsert;
 export type SelectUser = typeof users.$inferSelect;
 
 export type InsertRole = typeof roles.$inferInsert;
 export type SelectRole = typeof roles.$inferSelect;
+
+export type InsertProduct = typeof product.$inferInsert;
+export type SelectProduct = typeof product.$inferSelect;
+
+export type InsertCategory = typeof category.$inferInsert;
+export type SelectCategory = typeof category.$inferSelect;
+
+export type InsertOrder = typeof order.$inferInsert;
+export type SelectOrder = typeof order.$inferSelect;
